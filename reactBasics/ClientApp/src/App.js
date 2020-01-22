5a24411a2b1b59b0e0bbd69936233ce6dd073b31
kristoffer <zyprezz@gmail.com>
2020-01-22 21:18:20 +0100

    4:32

--- reactBasics/ClientApp/src/App.js
+++ reactBasics/ClientApp/src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-
+import StatelessComponent from './components/stateless-component';
 import './custom.css'
 
 export default class App extends Component {
@@ -12,11 +7,13 @@ export default class App extends Component {
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+        <StatelessComponent
+            props_1="hello im prop 1"
+            props_2={12}
+            props_3={false}
+            props_4={{ key: "iamthekey", value: "iamthevalue" }}
+            
+        />
     );
   }
 }
---
+++ reactBasics/ClientApp/src/components/stateless-component.js
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+
+export default function StatelessComponent(props) {
+
+    return <ul>
+        <li>{props[0]} </li>
+    </ul>
+}
