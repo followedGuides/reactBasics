f4e1a12709f597e9e187a1680002479c85d0966e
kristoffer <zyprezz@gmail.com>
2020-01-23 20:18:06 +0100

    4:32

--- reactBasics/ClientApp/src/components/stateless-component.js
+++ reactBasics/ClientApp/src/components/stateless-component.js
@@ -2,8 +2,13 @@
 
 
 export default function StatelessComponent(props) {
-
-    return <ul>
-        <li>{props[0]} </li>
+    console.log(props)
+    return(
+    <ul>
+        <li>{props.props_1} </li>
+        <li>{props.props_2} </li>
+        <li>{props.props_3} </li>
+        <li>{JSON.stringify(props.props_4)} </li>
     </ul>
+        )
 }
