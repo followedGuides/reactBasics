b2457301d43e9185741d34625595a75e06c1ff3f
zyprezz@gmail.com <zyprezz@gmail.com>
2020-01-24 20:50:39 +0100

    19:07

--- reactBasics/ClientApp/src/components/statefull-component.js
+++ reactBasics/ClientApp/src/components/statefull-component.js
@@ -4,17 +4,27 @@ class StatefullComponent extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            ...props
+            items: props.items
         }
     }
 
+    addItem(item) {
+        this.setState({
+            items: [...this.state.items, item ]
+        })
+    }
+
     render() {
-        return(
+        return (
+            <div>
         <ul>
             {
                 this.state.items.map(item => <li> {item} </li>)
             }
         </ul>
+                <input type="text" placeholder="type a new item in here" />
+                <button> add item to list</button>
+            </div>
     )}
 }
 
