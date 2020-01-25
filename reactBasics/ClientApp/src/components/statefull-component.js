6b36288b1941c0b3a26284c90c5ee791f211f49c
zyprezz@gmail.com <zyprezz@gmail.com>
2020-01-25 10:58:39 +0100

    19:07

--- reactBasics/ClientApp/src/components/statefull-component.js
+++ reactBasics/ClientApp/src/components/statefull-component.js
@@ -4,9 +4,20 @@ class StatefullComponent extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            items: props.items
+            items: props.items,
+            newItem: '',
         }
+        this.addItem = this.addItem.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
+
+    handleChange(e) {
+        console.log(e.target)
+        this.setState({
+            newItem: e.target.value
+        })
+    }
+
 
     addItem(item) {
         this.setState({
@@ -22,7 +33,8 @@ class StatefullComponent extends React.Component {
                 this.state.items.map(item => <li> {item} </li>)
             }
         </ul>
-                <input type="text" placeholder="type a new item in here" />
+                <input type="text" placeholder="type a new item in here" value={this.state.newItem} onChange={this.handleChange}
+                    />
                 <button> add item to list</button>
             </div>
     )}
